0ebe6da18e5823df495b3817c0b8590b33329987
Arijit 365 <banerjeearijit365@fmail.com>
2025-02-05 23:48:29 +0530

    changes

---
+++ client/client-dashboard/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import './App.css'
+import LoginSignup from './Components/LoginSignup/LoginSignup'
+import RegistrationSuccess from './Pages/RegistrationSuccess'
+import MainDashboard from './Components/MainDashboard/MainDashboard'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<LoginSignup/>}/>
+      <Route path='/success' element={<RegistrationSuccess/>}/>
+<Route path='/main-page' element={<MainDashboard/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
